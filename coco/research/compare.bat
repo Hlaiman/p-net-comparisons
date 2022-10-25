f90b51d50c3616678d2314e04e71541ddde209fc
Dmitriy Karpenko <dmitriy.qua@gmail.com>
2022-10-25 11:27:02 +0300

    remove coco init

--- coco/research/compare.bat
+++
@@ -1,7 +0,0 @@
-@if EXIST ..\..\bin\Python37\python.exe SET PythonPath=..\..\bin\Python37
-@if NOT EXIST %PythonPath%\python.exe goto exit
-
-%PythonPath%\python compare.py
-
-pause
-:exit
--- coco/research/train_pnet.bat
+++
@@ -1,7 +0,0 @@
-@if EXIST ..\..\bin\Python37\python.exe SET PythonPath=..\..\bin\Python37
-@if NOT EXIST %PythonPath%\python.exe goto exit
-
-%PythonPath%\python train_pnet.py
-
-pause
-:exit
